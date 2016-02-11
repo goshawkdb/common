90b952ec4f96795ffa64456850b9d3e61ce60a25
Matthew Sackman <matthew@goshawkdb.io>
2016-02-11 22:46:23 +0000

    This appears to make the built deb completely deterministic. Not sure the zero epochs are a good idea though. Ref T14.
    --HG--
    branch : T14

--- packaging/nix/0.1/builder-deb.sh
+++ packaging/nix/0.1/builder-deb.sh
@@ -4,6 +4,21 @@ cp $debian/DEBIAN/control $TMP/debian/DEBIAN
 chmod 755 $TMP/debian/DEBIAN
 mkdir -p $TMP/debian/usr
 cp -r $server/bin $TMP/debian/usr/
+cd $TMP/debian
+md5sum $(find usr -depth -type f | sort) > $TMP/debian/DEBIAN/md5sums
+cd $TMP
 chmod -R 755 $TMP/debian/usr/bin
+for t in $(find $TMP/debian -depth); do
+  touch -at 197001010000 $t
+  touch -mt 197001010000 $t
+done
+mkdir -p $TMP/deb
+fakeroot -- dpkg-deb --build debian deb
+name=$(ls $TMP/deb)
+ar x $TMP/deb/$name
+for t in $(ar t $TMP/deb/$name); do
+  touch -at 197001010000 $t
+  touch -mt 197001010000 $t
+done
 mkdir -p $out
-fakeroot -- dpkg-deb --build debian $out
+ar cq $out/$name $(ar t $TMP/deb/$name)
