a05b6c5632a09277ecd5dfb2529bb905a8a7b029
Matthew Sackman <matthew@goshawkdb.io>
2016-02-18 13:49:16 +0000

    Add debian changelog file. Ref T14.
    --HG--
    branch : T14

--- packaging/nix/0.1/builder-deb.sh
+++ packaging/nix/0.1/builder-deb.sh
@@ -9,16 +9,23 @@ cd $TMP
 mkdir -p $TMP/debian/DEBIAN
 cp $debian/DEBIAN/control $TMP/debian/DEBIAN
 chmod 755 $TMP/debian/DEBIAN
+
 mkdir -p $TMP/debian/usr/share/doc/goshawkdb-server
 cp $debian/copyright $TMP/debian/usr/share/doc/goshawkdb-server/copyright
 chmod 644 $TMP/debian/usr/share/doc/goshawkdb-server/copyright
 cat $TMP/src/$license | sed -e 's/^$/./; s/^/ /;' >> $TMP/debian/usr/share/doc/goshawkdb-server/copyright
+cp $debian/changelog.Debian $TMP/debian/usr/share/doc/goshawkdb-server/changelog.Debian
+touch -ad "${timestamp}" $TMP/debian/usr/share/doc/goshawkdb-server/changelog.Debian
+touch -md "${timestamp}" $TMP/debian/usr/share/doc/goshawkdb-server/changelog.Debian
+gzip --best $TMP/debian/usr/share/doc/goshawkdb-server/changelog.Debian
+chmod 644 $TMP/debian/usr/share/doc/goshawkdb-server/changelog.Debian.gz
+
 cp -r $server/bin $TMP/debian/usr/
 cd $TMP/debian
 md5sum $(find usr -depth -type f | sort) > $TMP/debian/DEBIAN/md5sums
 cd $TMP
 chmod -R 755 $TMP/debian/usr/bin
-for t in $(find $TMP/debian -depth); do
+for t in $(find $TMP/debian -depth | sort); do
   touch -ad "${timestamp}" $t
   touch -md "${timestamp}" $t
 done
