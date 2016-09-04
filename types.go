cc44481806884d623de8d1284347e1ea21ead6b2
Matthew Sackman <matthew@goshawkdb.io>
2016-09-04 16:58:58 +0100

    Extract out some common functionality. Ref T34.
    --HG--
    branch : T34

--- types.go
+++ types.go
@@ -226,7 +226,27 @@ func (rmIds RMIds) NonEmpty() RMIds {
 	return nonEmpty
 }
 
-func EqualCapabilities(a, b *msgs.Capabilities) bool {
+var MaxCapsCap *Capabilities
+
+func init() {
+	seg := capn.NewBuffer(nil)
+	cap := msgs.NewCapabilities(seg)
+	cap.SetValue(msgs.VALUECAPABILITY_READWRITE)
+	ref := cap.References()
+	ref.Read().SetAll()
+	ref.Write().SetAll()
+	MaxCapsCap = NewCapabilities(cap)
+}
+
+type Capabilities struct {
+	msgs.Capabilities
+}
+
+func NewCapabilities(c msgs.Capabilities) *Capabilities {
+	return &Capabilities{Capabilities: c}
+}
+
+func (a *Capabilities) Equal(b *Capabilities) bool {
 	if a.Value() != b.Value() {
 		return false
 	}
@@ -258,6 +278,130 @@ func EqualCapabilities(a, b *msgs.Capabilities) bool {
 		}
 	}
 	return true
+}
+
+func (c *Capabilities) String() string {
+	value := ""
+	switch c.Value() {
+	case msgs.VALUECAPABILITY_NONE:
+		value = "None"
+	case msgs.VALUECAPABILITY_READ:
+		value = "Read"
+	case msgs.VALUECAPABILITY_WRITE:
+		value = "Write"
+	case msgs.VALUECAPABILITY_READWRITE:
+		value = "ReadWrite"
+	}
+	refsRead := ""
+	if c.References().Read().Which() == msgs.CAPABILITIESREFERENCESREAD_ALL {
+		refsRead = "All"
+	} else {
+		refsRead = fmt.Sprintf("%v", c.References().Read().Only().ToArray())
+	}
+	refsWrite := ""
+	if c.References().Write().Which() == msgs.CAPABILITIESREFERENCESWRITE_ALL {
+		refsWrite = "All"
+	} else {
+		refsWrite = fmt.Sprintf("%v", c.References().Write().Only().ToArray())
+	}
+	return fmt.Sprintf("{capability: value: %s; refsRead: %s; refsWrite: %s}", value, refsRead, refsWrite)
+}
+
+func (a *Capabilities) Union(b *Capabilities) *Capabilities {
+	switch {
+	case a == b:
+		return a
+	case a == MaxCapsCap || b == MaxCapsCap:
+		return MaxCapsCap
+	case a == nil:
+		return b
+	case b == nil:
+		return a
+	}
+
+	aValue := a.Value()
+	aRefsRead := a.References().Read()
+	aRefsWrite := a.References().Write()
+
+	bValue := b.Value()
+	bRefsRead := b.References().Read()
+	bRefsWrite := b.References().Write()
+
+	valueRead := aValue == msgs.VALUECAPABILITY_READWRITE || aValue == msgs.VALUECAPABILITY_READ ||
+		bValue == msgs.VALUECAPABILITY_READWRITE || bValue == msgs.VALUECAPABILITY_READ
+	valueWrite := aValue == msgs.VALUECAPABILITY_READWRITE || aValue == msgs.VALUECAPABILITY_WRITE ||
+		bValue == msgs.VALUECAPABILITY_READWRITE || bValue == msgs.VALUECAPABILITY_WRITE
+	refsReadAll := aRefsRead.Which() == msgs.CAPABILITIESREFERENCESREAD_ALL || bRefsRead.Which() == msgs.CAPABILITIESREFERENCESREAD_ONLY
+	refsWriteAll := aRefsWrite.Which() == msgs.CAPABILITIESREFERENCESWRITE_ALL || bRefsWrite.Which() == msgs.CAPABILITIESREFERENCESWRITE_ALL
+
+	if valueRead && valueWrite && refsReadAll && refsWriteAll {
+		return MaxCapsCap
+	}
+
+	seg := capn.NewBuffer(nil)
+	cap := msgs.NewCapabilities(seg)
+	switch {
+	case valueRead && valueWrite:
+		cap.SetValue(msgs.VALUECAPABILITY_READWRITE)
+	case valueWrite:
+		cap.SetValue(msgs.VALUECAPABILITY_WRITE)
+	case valueRead:
+		cap.SetValue(msgs.VALUECAPABILITY_WRITE)
+	default:
+		cap.SetValue(msgs.VALUECAPABILITY_NONE)
+	}
+
+	if refsReadAll {
+		cap.References().Read().SetAll()
+	} else {
+		aOnly, bOnly := aRefsRead.Only().ToArray(), bRefsRead.Only().ToArray()
+		cap.References().Read().SetOnly(mergeOnliesSeg(seg, aOnly, bOnly))
+	}
+
+	if refsWriteAll {
+		cap.References().Write().SetAll()
+	} else {
+		aOnly, bOnly := aRefsWrite.Only().ToArray(), bRefsWrite.Only().ToArray()
+		cap.References().Write().SetOnly(mergeOnliesSeg(seg, aOnly, bOnly))
+	}
+
+	return NewCapabilities(cap)
+}
+
+func mergeOnliesSeg(seg *capn.Segment, a, b []uint32) capn.UInt32List {
+	only := mergeOnlies(a, b)
+
+	cap := seg.NewUInt32List(len(only))
+	for idx, index := range only {
+		cap.Set(idx, index)
+	}
+	return cap
+}
+
+func mergeOnlies(a, b []uint32) []uint32 {
+	only := make([]uint32, 0, len(a)+len(b))
+	for len(a) > 0 && len(b) > 0 {
+		aIndex, bIndex := a[0], b[0]
+		switch {
+		case aIndex < bIndex:
+			only = append(only, aIndex)
+			a = a[1:]
+		case aIndex > bIndex:
+			only = append(only, bIndex)
+			b = b[1:]
+		default:
+			only = append(only, aIndex)
+			a = a[1:]
+			b = b[1:]
+		}
+	}
+	if len(a) > 0 {
+		only = append(only, a...)
+	} else {
+		only = append(only, b...)
+	}
+
+	return only
 }
 
 type SortUInt32 []uint32
