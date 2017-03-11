fa95154f51195bea3b86722289bd3a60c6cd9299
Matthew Sackman <matthew@goshawkdb.io>
2017-03-11 21:38:11 +0000

    Add prometheus port to consts. Ref T83
    --HG--
    branch : T83

--- consts.go
+++ consts.go
@@ -5,12 +5,13 @@ import (
 )
 
 const (
-	DefaultPort          = 7894
-	DefaultWSSPort       = 7895
-	ProductVersion       = "dev" // protocol version!
-	ProductName          = "GoshawkDB"
-	HeartbeatInterval    = 2 * time.Second
-	ConnectionBufferSize = 131072
+	DefaultPort           = 7894
+	DefaultPrometheusPort = 8080
+	DefaultWSSPort        = 7895
+	ProductVersion        = "dev" // protocol version!
+	ProductName           = "GoshawkDB"
+	HeartbeatInterval     = 2 * time.Second
+	ConnectionBufferSize  = 131072
 )
 
 var (
